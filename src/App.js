3763ba35e2748ea3eb5699ed1d302014196d4b28
HEMANTHZ10 <peralahemanth10@gmail.com>
2024-05-23 12:52:44 +0530

    added form with search

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import UserHome from './components/userhome/userhome';
 import AdminHome from './components/adminhome/adminhome';
 import UploadData from './components/uploadData/uploadData';
+import Searchdata from './components/searchdata/searchdata';
 function App() {
   return (
     <div className="App">
-      <UploadData />
+      <Searchdata />
     </div>
   );
 }
