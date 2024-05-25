e10c51b11283dfe35d2606dc7115d3479f047b40
HEMANTHZ10 <peralahemanth10@gmail.com>
2024-05-25 13:29:51 +0530

    updated the user and admin pages with the sidebar functionality

--- src/components/adminhome/adminhome.js
+++ src/components/adminhome/adminhome.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './adminhome.css';
 
 function AdminHome() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="adminhome">
       <header className="header">
         <div className="container-fluid">
           <div className="flex">
+            <div className="menu-icon" onClick={toggleSidebar}>
+              <img src="https://icon-library.com/images/menu-icon-white-png/menu-icon-white-png-27.jpg" className="menu-img" alt="Menu Icon" />
+            </div>
             <div className="logo-container">
               <img src="https://upload.wikimedia.org/wikipedia/en/4/47/VNRVJIETLogo.png" className="logo" alt="VNRVJIET Logo" />
             </div>
@@ -16,40 +25,21 @@ function AdminHome() {
           </div>
           <div className="rightside">
             <div className="admin-info">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPXG-9c1j983Z3EAwScbiKGnII2UoAEgfZsPPEDvrA0A&s" className="admin-photo" alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPXG-9c1j983Z3EAwScbiKGnII2UoAEgfZsPPEDvrA0A&s" className="admin-photo" alt="Admin" />
               <span className="adminname">Admin Name</span>
             </div>
           </div>
         </div>
       </header>
-      <div className="buttons">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <button className="btn">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2VB8iYVjVMtOvYI6sT6gFbHbBiQyxN7UNbwrtFON4fw&s" alt="lecture" />
-                <span>Lecture Progress</span>
-              </button>
-            </div>
-            <div className="col">
-              <button className="btn">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo2TbWxiPdaRrjANdWpUZ1_K4f__nUIE_6lMMBqkOrUQ&s" alt="yearwise" />
-                <span>AY-wise Report</span>
-              </button>
-            </div>
-            <div className="col">
-              <button className="btn">
-                <img src=" https://previews.123rf.com/images/stuartphoto/stuartphoto1906/stuartphoto190600839/124929722-executive-summary-text-icon-showing-short-condensed-report-roundup-3d-illustration-summing-up.jpg  " className='sr' alt="summary" />
-                <span>Summary Report</span>
-              </button>
-            </div>
-        </div>
+
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <button className="sidebar-btn">Lecture Progress</button>
+        <button className="sidebar-btn">AY-wise Report</button>
+        <button className="sidebar-btn">Summary Report</button>
       </div>
-</div>
+
+      <div className={`overlay ${sidebarOpen ? 'show' : ''}`} onClick={toggleSidebar}></div>
     </div>
-
-
-
   );
 }
 
--- src/components/userhome/userhome.js
+++ src/components/userhome/userhome.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './userhome.css';
 
 function UserHome() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <div className="userhome">
       <header className="header">
         <div className="container-fluid">
           <div className="flex">
+            <div className="menu-icon" onClick={toggleSidebar}>
+              <img src="https://icon-library.com/images/menu-icon-white-png/menu-icon-white-png-27.jpg" className="menu-img" alt="Menu Icon" />
+            </div>
             <div className="logo-container">
               <img src="https://upload.wikimedia.org/wikipedia/en/4/47/VNRVJIETLogo.png" className="logo" alt="VNRVJIET Logo" />
             </div>
@@ -16,43 +25,21 @@ function UserHome() {
           </div>
           <div className="rightside">
             <div className="user-info">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPXG-9c1j983Z3EAwScbiKGnII2UoAEgfZsPPEDvrA0A&s" className="user-photo" alt="" />
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPXG-9c1j983Z3EAwScbiKGnII2UoAEgfZsPPEDvrA0A&s" className="user-photo" alt="User" />
               <span className="username">Sample Name</span>
             </div>
           </div>
         </div>
       </header>
-      <div className="buttons">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <button className="btn">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfbKYW1bf5yHlEY3-IhLEa8yOvDUrZKzTl64Zf5X22cYrstqyg4SFs0_GY198WY7qzvj8&usqp=CAU" alt="Upload" />
-                <span>Upload</span>
-              </button>
-            </div>
-            <div className="col">
-              <button className="btn">
-                <img src="https://media.istockphoto.com/id/1491754746/vector/business-growth-chart-logo-with-arrow-bar-and-line-chart-diagram.jpg?s=612x612&w=0&k=20&c=kfZKV3WUirRRb_dEbda8mUmbTQmgiPdApeTwRDwt1ew=" alt="Dashboard" />
-                <span>Dashboard</span>
-              </button>
-            </div>
-         
-            <div className="col">
-              <button className="btn">
-                <img src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRULhDp4W-He4wkiNSBYuMRLzfC5PFJ7zIORWsDLpHSQ&s" alt="Activity" />
-                <span>Activity</span>
-              </button>
-            </div>
-            
-          </div>
-        </div>
+
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <button className="sidebar-btn">Upload</button>
+        <button className="sidebar-btn">Dashboard</button>
+        <button className="sidebar-btn">Activity</button>
       </div>
 
+      <div className={`overlay ${sidebarOpen ? 'show' : ''}`} onClick={toggleSidebar}></div>
     </div>
-
-
-
   );
 }
 
