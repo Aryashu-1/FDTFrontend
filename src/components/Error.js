f522e525d670bdb2bf748df23c487097236367a0
RoHiT-48566 <159615344+RoHiT-48566@users.noreply.github.com>
2024-06-16 12:02:07 +0530

    Add files via upload

---
+++ src/components/Error.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Error.css";
+
+import errorImage from "../../assets/Error.jpg";
+
+export default function Error() {
+  return <img src={errorImage} alt="Oops 404! page not found" />;
+}
